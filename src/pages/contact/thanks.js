71548de6eb677596b1cc9d6ce0502eb90abd2757
Long <chuanman2707@gmail.com>
2019-10-02 08:16:25 +0700

    update logo

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import Layout from '../../components/Layout'
+import React from "react";
+import Layout from "../../components/Layout";
 
 export default () => (
   <Layout>
@@ -7,9 +7,12 @@ export default () => (
       <div className="container">
         <div className="content">
           <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <p>
+            Cảm ơn bạn đã liên hệ, tôi sẽ trả lời bạn trong vòng 24 tiếng đồng
+            hồ.
+          </p>
         </div>
       </div>
     </section>
   </Layout>
-)
+);
